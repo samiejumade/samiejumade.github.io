6b6587ccaacdd1e2b335effd2676779cec93b741
Samir Jumade <samiejumade@gmail.com>
2026-04-05 17:55:46 +0530

    update resume_links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@
  * Update these URLs to change the resume without redeploying the site.
  */
 export const RESUME_LINKS = {
-  VIEW: "https://drive.google.com/file/d/1WLOQSb6SzeaAG30y3wU20IYExz21fDz9/view",
+  VIEW: "https://drive.google.com/file/d/1uCEkulcDAKACxqfsud1FCp1xcfiNG8Rj/view",
   DOWNLOAD:
-    "https://drive.google.com/uc?export=download&id=1WLOQSb6SzeaAG30y3wU20IYExz21fDz9",
+    "https://drive.google.com/uc?export=download&id=1uCEkulcDAKACxqfsud1FCp1xcfiNG8Rj",
 } as const;
