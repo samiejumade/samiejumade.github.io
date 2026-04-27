dc8c3ee8f0cdb771f4eb8bee711354e28164313f
Samir Jumade <samiejumade@gmail.com>
2026-04-27 15:29:19 +0530

    Update resume links to latest version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,9 +3,9 @@
  * Update these URLs to change the resume without redeploying the site.
  */
 export const RESUME_LINKS = {
-  VIEW: "https://drive.google.com/file/d/1uCEkulcDAKACxqfsud1FCp1xcfiNG8Rj/view",
+  VIEW: "https://drive.google.com/file/d/1xVUoQO8jwDtKQk0tNigy-KQGDVG8Q-1V/view",
   DOWNLOAD:
-    "https://drive.google.com/uc?export=download&id=1uCEkulcDAKACxqfsud1FCp1xcfiNG8Rj",
+    "https://drive.google.com/uc?export=download&id=1xVUoQO8jwDtKQk0tNigy-KQGDVG8Q-1V",
 } as const;
 
 export const BLOG_FEED_URL = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@samir.woxsen";
